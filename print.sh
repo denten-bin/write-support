078efe9f57ebcfb8149fae7a750d794cf06fe896
denten <denten@gmail.com>
2015-11-18 11:39:29 -0500

    updating print

--- print.sh
+++ print.sh
@@ -16,27 +16,37 @@ fi
 
 source=$2
 
-echo $2
-
 # Use parameter expansion to strip the name
 target="${source%%.*}"
+
+# check if the .yaml is in separate file
+# this is nice for when the same files share the same yaml header
+# if not just stuff with a harmless duplicate --verbose
+if test -f metadata.yaml
+then
+    yaml="metadata.yaml"
+else
+    yaml="--verbose"
+fi
 
 # handle two options with getopts: -d for .docx and -p for .pdf
 
 while getopts ":d:p:m:" opt; do
   case $opt in
     d)
-      echo "printing $target.docx to print-plates/" >&2
-      pandoc -s "$source" pass-ins/metadata.yaml \
+      echo "printing $source to print-plates/$target.docx " >&2
+      pandoc -s "$source" "$yaml" \
           --verbose \
+          --normalize \
           --filter pandoc-citeproc \
           --csl ~/bin/write-support/csl/mla-note.csl \
           -So print-plates/"$target".docx
       ;;
     p)
-      echo "printing $target.pdf to print-plates/" >&2
-      pandoc -s "$source" pass-ins/metadata.yaml \
+      echo "printing $source to print-plates/$target.pdf " >&2
+      pandoc -s "$source" "$yaml" \
           --verbose \
+          --normalize \
           --latex-engine=xelatex \
           --filter pandoc-citeproc \
           --csl ~/bin/write-support/csl/mla-no-biblio.csl \
@@ -45,6 +55,8 @@ while getopts ":d:p:m:" opt; do
     m)
       echo "printing $target.md to print-plates/" >&2
       pandoc "$source" \
+          --verbose \
+          --normalize \
           -f markdown \
           -t markdown+hard_line_breaks \
           -So print-plates/"$target".md
